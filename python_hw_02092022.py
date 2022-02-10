6e291ff70befdaec49d09855e13885ef72accf64
Felipe AKA Chewie <99052148+fdgalvan@users.noreply.github.com>
2022-02-10 09:39:13 -0600

    I put # on all lines.

--- python_hw_02092022.py
+++ python_hw_02092022.py
@@ -33,29 +33,29 @@
 
 #4 Day of the Week
 
-print("Hello, please select a day of the week from 0-6. ")
+#print("Hello, please select a day of the week from 0-6. ")
 
-day = int(input('Day (0-6)? '))
+#day = int(input('Day (0-6)? '))
 
-if input == 0:
-    result = "Sunday"
-elif input == 1:
-    result = "Monday"
-elif input == 2:
-    result = "Tuesday"
-elif input == 3:
-    result = "Wednesday"
-elif input == 4:
-    result = "Thursday"
-elif input == 5:
-    result = "Friday"
-elif input == 6:
-    result = "Saturday"
-else:
-    result = "that's not a whole number between zero and six, buddy."
-    
-    print(result)
-    
+#if input == 0:
+#    result = "Sunday"
+#elif input == 1:
+#    result = "Monday"
+#elif input == 2:
+#    result = "Tuesday"
+#elif input == 3:
+#    result = "Wednesday"
+#elif input == 4:
+#    result = "Thursday"
+#elif input == 5:
+#    result = "Friday"
+#elif input == 6:
+#    result = "Saturday"
+#else:
+#    result = "that's not a whole number between zero and six, buddy."
+#    
+#    print(result)
+#    
 # I feel like I am so very close!
 
 #------------------------------------------
