f730bca0b46aae57f051270808954adc8137d805
Felipe AKA Chewie <99052148+fdgalvan@users.noreply.github.com>
2022-02-10 09:16:16 -0600

    hw01_02092022

---
+++ python_hw_02092022.py
@@ -0,0 +1,64 @@
+#1 Hello You! 
+
+#name = input("What is your name?")
+
+#print("Hello,",name,"!")
+
+#------------------------------------------
+
+#2 HELLO YOU!
+
+#name = input("WHAT IS YOUR NAME?")
+
+#name.upper()
+
+#print("HELLO, ",name.upper(),"!")
+#print("YOUR NAME CONTAINS", len(name), "LETTERS IN IT! AWESOME!")
+
+#------------------------------------------
+
+#3 Madlib
+
+#name = input("First, please give me a name.")
+
+#subject = input("Now, please give me a subject.")
+
+#name_input = name
+
+#subject_input = subject
+
+#print(name_input,"'s","favorite subject in school is",subject_input,".")
+
+#------------------------------------------
+
+#4 Day of the Week
+
+print("Hello, please select a day of the week from 0-6. ")
+
+day = int(input('Day (0-6)? '))
+
+if input == 0:
+    result = "Sunday"
+elif input == 1:
+    result = "Monday"
+elif input == 2:
+    result = "Tuesday"
+elif input == 3:
+    result = "Wednesday"
+elif input == 4:
+    result = "Thursday"
+elif input == 5:
+    result = "Friday"
+elif input == 6:
+    result = "Saturday"
+else:
+    result = "that's not a whole number between zero and six, buddy."
+    
+    print(result)
+    
+# I feel like I am so very close!
+
+#------------------------------------------
+
+#5 Work or Sleep In?
+
