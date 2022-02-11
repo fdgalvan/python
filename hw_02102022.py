7421ecde306f1b844dcffcb2c96fcef13eec1d15
Felipe AKA Chewie <99052148+fdgalvan@users.noreply.github.com>
2022-02-11 09:26:10 -0600

    hw 02102022

---
+++ hw_02102022.py
@@ -0,0 +1,41 @@
+#Python HW 02102022
+
+#1 sum of numbers
+
+nums = [1, 2, 3 ,4]
+sum = sum(nums)
+print(sum)
+
+#------------------------
+
+#2 largest number
+
+list1 = [1, 2, 3, 4]
+
+print("Largest element is:", list1[-1])
+
+#------------------------
+
+#3 smallest number
+
+list1 = [1, 2, 3, 4]
+
+print("smallest element is:", list1[:1])
+
+#------------------------
+
+#4 even numbers
+
+nums1 = [1, 2, 3, 4]
+
+for num in nums1:
+    if num % 2 == 0:
+        print(num, end=" ")
+
+#------------------------
+
+#5 numbers greater than zero
+
+#6 positive numbers
+
+#7 multiply numbers by a single factor
---
+++ hw_revision_of_02092022.py
@@ -0,0 +1,43 @@
+#4 Day of the Week
+
+#print("Hello, please select a day of the week from 0-6. ")
+
+#day = int(input('Day (0-6)? '))
+
+#if input == 0:
+#    print("Sunday")
+#elif input == 1:
+#    print("Monday")
+#elif input == 2:
+#    print("Tuesday")
+#elif input == 3:
+#    print("Wednesday")
+#elif input == 4:
+#    print("Thursday")
+#elif input == 5:
+#    print("Friday")
+#elif input == 6:
+#    print("Saturday")
+#else:
+#    print("that's not a whole number between zero and six, buddy.")
+#    
+
+# I feel like I am so very close!
+
+#------------------------------------------
+
+#7 Looping from 1 to 10
+
+#num = 0
+#while num < 10:
+#    num += 1
+#    print(num)
+#    
+#------------------------------------------
+
+#9
+
+count=0
+while count <5:
+    print("*****")
+    count+=1
